6eca4672e13897bf14974ace58728d699650ab82
John-Ristum <125324914+John-Ristum@users.noreply.github.com>
2024-06-26 18:06:25 +1000

    Week 4 Class - UI

---
+++ Assets/Universal/Scripts/SceneController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadScene(string _sceneName) => SceneManager.LoadScene(_sceneName);
+
+    public void ReloadScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    public void LoadTitle() => LoadScene("Title");
+
+    public void Quit() => Application.Quit();
+}
---
+++ Assets/Universal/Scripts/TitleButton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleButton : MonoBehaviour
+{
+    public Image splashImage;
+    public Sprite mySplashImage;
+
+    public void OnMouseEnter()
+    {
+        splashImage.sprite = mySplashImage;
+        splashImage.enabled = true;
+    }
+
+    public void OnMouseExit()
+    {
+        splashImage.enabled = false;
+    }
+}
