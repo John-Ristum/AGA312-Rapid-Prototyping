738941127f95878bddbdae9131416bcd82398841
John-Ristum <125324914+John-Ristum@users.noreply.github.com>
2024-08-21 00:30:00 +1000

    Variable Jump Height & Dashing

--- Assets/Prototype5/Scripts/PlayerMovementPT5.cs
+++ Assets/Prototype5/Scripts/PlayerMovementPT5.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerMovementPT5 : MonoBehaviour
 {
+    public enum PlayerState { Normal, Dashing }
+    public PlayerState state;
+
     public CharacterController controller;
-    public float speed = 6f;
+    float speed;
+    public float walkSpeed = 6f;
+    public float dashSpeed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
+    bool jumping;
     public Transform startPoint;
 
     public Transform groundCheck;
@@ -22,26 +28,45 @@ public class PlayerMovementPT5 : MonoBehaviour
 
     float x;
     float y;
-    float xRaw;
-    float yRaw;
+    float yOffset;
+    //float xRaw;
+    //float yRaw;
     Vector3 targetRotation;
     public float rotationSpeed = 5;
     bool falling;
+    bool canJump;
 
     // Start is called before the first frame update
     void Start()
     {
+        state = PlayerState.Normal;
         controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Player Speed
+        //switch (state)
+        //{
+        //    case PlayerState.Normal:
+        //        speed = walkSpeed;
+        //        break;
+        //    case PlayerState.Dashing:
+        //        speed = dashSpeed;
+        //        break;
+        //}
+
         //Get the input from the player
-        x = Input.GetAxis("Horizontal");
-        y = Input.GetAxis("Vertical");
-        xRaw = Input.GetAxisRaw("Horizontal");
-        yRaw = Input.GetAxisRaw("Vertical");
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+        //xRaw = Input.GetAxisRaw("Horizontal");
+        //yRaw = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isGrounded)
+            state = PlayerState.Dashing;
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            state = PlayerState.Normal;
 
         //Checks if we are touching the ground
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -50,22 +75,57 @@ public class PlayerMovementPT5 : MonoBehaviour
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
+            canJump = true;
             falling = false;
         }
 
-        if (x == 0 && y == 0 || falling)
+        switch (state)
         {
-            velocity.y += gravity * Time.deltaTime;
-            controller.Move(velocity * Time.deltaTime);
+            case PlayerState.Normal:
+                velocity.y += gravity * Time.deltaTime;
+                controller.Move(velocity * Time.deltaTime);
+                break;
+            case PlayerState.Dashing:
+                if (x == 0 || falling)
+                {
+                    velocity.y += gravity * Time.deltaTime;
+                    controller.Move(velocity * Time.deltaTime);
+                }
+                break;
         }
+
+        
+
+        //if (x == 0 && y == 0 || falling)
+        //{
+        //    velocity.y += gravity * Time.deltaTime;
+        //    controller.Move(velocity * Time.deltaTime);
+        //}
 
         if (velocity.y < -2.1)
             falling = true;
-            //Debug.Log("falling");
+        //Debug.Log("falling");
 
         //Move the player
         Vector3 move = transform.right * x + transform.forward * y;
-        controller.Move(move * speed * Time.deltaTime);
+        if (x == 0)
+            yOffset = 1;
+        else
+            yOffset = 0.75f;
+
+        switch (state)
+        {
+            case PlayerState.Normal:
+                controller.Move(move.normalized * walkSpeed * Time.deltaTime);
+                break;
+            case PlayerState.Dashing:
+                controller.Move((transform.right * x) * dashSpeed * Time.deltaTime);
+                controller.Move((transform.forward * y) * (walkSpeed * yOffset) * Time.deltaTime);
+                break;
+        }
+        
+
+        
 
         //Vector3 move = new Vector3(x, 0, y);
         //Vector3 moveRaw = new Vector3(xRaw, 0, yRaw);
@@ -96,10 +156,22 @@ public class PlayerMovementPT5 : MonoBehaviour
         //Does the jump stuff
         if (Input.GetButtonDown("Jump"))
         {
-            if (isGrounded || !falling)
-                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            falling = true;
+            Jump();
         }
+        if (Input.GetButtonUp("Jump") && velocity.y > -0.2)
+        {
+            velocity.y /= 2;
+        }
+    }
+
+    void Jump()
+    {
+        if (canJump)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            canJump = false;
+        }
+        falling = true;
     }
 
     private void Flip()
@@ -111,5 +183,7 @@ public class PlayerMovementPT5 : MonoBehaviour
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+
+        falling = true;
     }
 }
